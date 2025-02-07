d01727dde1164564144a0cc5f33a218ed9961197
jeryn <81574573+jerynv@users.noreply.github.com>
2025-02-07 10:35:12 -0500

    minor updates

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,11 +1,69 @@
+class Documentaion {
+    //take in a json of the documentation with titles and other information in a list representing a vertical layout of the documentation
+    constructor(selector) {
+        this.content = document.createElement("div");
+        this.parent = document.querySelector(selector);
+    }
+
+    init(obj) {
+        //the obj is a list of objects and i want to check each one for keys and value like  key "codeSnippet" and it will create html element with the key as the functions and the value as the content
+        for (const item of obj) {
+            for (const key in item) {
+                switch (key) {
+                    case "codeSnippet":
+                        this.createCodeSnippet(item[key]);
+                        break;
+                    case "text":
+                        this.createText(item[key]);
+                        break;
+                    case "title":
+                        this.createTitle(item[key]);
+                        break;
+                    case "list":
+                        this.createList(item[key]);
+                        break;
+                    case "image":
+                        this.createImage(item[key]);
+                        break;
+                    default:
+                        console.log("no key found");
+                        break;
+                }
+            }
+        }
+        this.render();
+    }
+
+    createCodeSnippet(code) {
+        const element = document.createElement("div");
+        element.classList.add("code-snippet");
+        element.innerHTML = code;
+        this.content.appendChild(element);
+    }
+
+    createTitle(text) {
+        const element = document.createElement("h1");
+        element.innerHTML = text;
+        this.content.appendChild(element);
+    }
+
+    render() {
+        this.parent.appendChild(this.content);
+    }
+
+    destroy() {
+        this.content.innerHTML = "";
+    }
+}
+
 class Docs {
-    constructor(obj, selector, load_selector) {
+    constructor(obj, selector, doc_controller) {
         this.obj = obj.tree;
         this.load_path = obj.load_path;
         this.load_extension = obj.load_extension;
         this.content = ``;
         this.parent = document.querySelector(selector);
-        this.load_destination = document.querySelector(load_selector);
+        this.doc_controller = doc_controller;
     }
 
     init() {
@@ -45,9 +103,9 @@ class Docs {
                 const childElement = document.createElement("div");
                 const childText = document.createTextNode(child);
                 if (this.obj[key].children[child].children) {
-                    console.log("nesting");
+                    //console.log("nesting");
                     childElement.appendChild(
-                        this.nest(this.obj[key].children[child], child)
+                        this.nest(this.obj[key].children[child], child, key)
                     );
                     childrenTree.appendChild(childElement);
                     continue;
@@ -66,7 +124,8 @@ class Docs {
                         }
                     }
                     childElement.classList.toggle("active");
-                    this.load(this.obj[key].children[child].page);
+                    //and the parent element name under the tree key
+                    this.load(key, this.obj[key].children[child].page);
                 });
             }
             element_base.appendChild(childrenTree);
@@ -75,13 +134,13 @@ class Docs {
         this.content = element;
     }
 
-    nest(obj, name) {
+    nest(obj, name, masterName) {
         //just create a minature dropdown to append to a dropdown
         const element = document.createElement("div");
         element.classList.add("nested-tree-element");
 
         for (const key in obj) {
-            console.log(key, " --key");
+            //console.log(key, " --key");
             const icon = document.createElement("svg");
             icon.innerHTML = `<svg class="icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 7C12.2652 7 12.5196 7.10536 12.7071 7.29289L19.7071 14.2929C20.0976 14.6834 20.0976 15.3166 19.7071 15.7071C19.3166 16.0976 18.6834 16.0976 18.2929 15.7071L12 9.41421L5.70711 15.7071C5.31658 16.0976 4.68342 16.0976 4.29289 15.7071C3.90237 15.3166 3.90237 14.6834 4.29289 14.2929L11.2929 7.29289C11.4804 7.10536 11.7348 7 12 7Z" fill="currentColor"></path> </g></svg>`;
             const text = document.createElement("p");
@@ -99,7 +158,9 @@ class Docs {
                 for (const item of active) {
                     if (
                         item !==
-                        element_base.querySelector(".nested-tree-child-container")
+                        element_base.querySelector(
+                            ".nested-tree-child-container"
+                        )
                     ) {
                         item.classList.remove("active");
                     }
@@ -111,7 +172,7 @@ class Docs {
             //console log the children but not in alphabetical order
 
             for (const child in obj[key]) {
-                console.log("child");
+                //console.log("child");
                 const childElement = document.createElement("div");
                 const childText = document.createTextNode(child);
                 childElement.classList.add("tree-child");
@@ -127,7 +188,8 @@ class Docs {
                         }
                     }
                     childElement.classList.toggle("active");
-                    this.load(obj[key].children[child].page);
+                    //console.log(masterName+name, child);
+                    this.load(`${masterName}/${name}`, child);
                 });
             }
             element_base.appendChild(childrenTree);
@@ -136,15 +198,19 @@ class Docs {
         return element;
     }
 
-    async load(page) {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", `${this.load_path}${page}${this.load_extension}`, true);
-        xhr.send();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                this.load_destination.innerHTML = xhr.responseText;
-            }
-        };
+    async load(parentName, page) {
+        parentName = parentName.replace(/\s/g, "").toLowerCase();
+        page = page.replace(/\s/g, "").toLowerCase();
+        let response = await fetch(
+            `${this.load_path}${parentName}/${page}.json`
+        );
+        try {
+            response = await response.json();
+        } catch (e) {
+            console.log("error loading json", e);
+        }
+        this.doc_controller.destroy();
+        this.doc_controller.init(response);
     }
 
     render() {
@@ -154,14 +220,35 @@ class Docs {
 
 const docs_template = {
     tree: {
-        intro: {
+        "pollchat hack": {
             children: {
-                Hello: { page: "Introduction" },
+                intro: {
+                    page: "intro",
+                },
+                navbhar: {
+                    children: {
+                        exponents: { page: "Navbar" },
+                        Headers: { page: "Headers" },
+                    },
+                },
+                navbar: {
+                    children: {
+                        exponents: { page: "Navbar" },
+                        Headers: { page: "Headers" },
+                    },
+                },
+                navbear: {
+                    children: {
+                        exponents: { page: "Navbar" },
+                        Headers: { page: "Headers" },
+                    },
+                },
+                footer: { page: "Footer" },
             },
         },
-        projects: {
+        "pollchat hack2": {
             children: {
-                navbar: {
+                intro: {
                     children: {
                         exponents: { page: "Navbar" },
                         Headers: { page: "Headers" },
@@ -173,7 +260,7 @@ const docs_template = {
                         Headers: { page: "Headers" },
                     },
                 },
-                navbaur: {
+                navbar: {
                     children: {
                         exponents: { page: "Navbar" },
                         Headers: { page: "Headers" },
@@ -190,9 +277,9 @@ const docs_template = {
         },
     },
     load_path: "../pages/components/docs/",
-    load_extension: ".xhr",
 };
 
-const docs = new Docs(docs_template, ".tree", ".docs-content");
+const docController = new Documentaion(".output");
+const docs = new Docs(docs_template, ".tree", docController);
 docs.init();
 docs.render();
